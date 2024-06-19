8d384e5b4f0f619b4695c0d1f249d183fb5a5d2e
sofia flores <bf917242@gmail.com>
2024-06-19 11:21:48 -0300

    cree la pagina de actores

---
+++ Programacion-Wed /arrays/arrays.js
@@ -0,0 +1,14 @@
+const array = [1, "strings", true]; //constitucion array
+
+const posicion = 1; //asignacion valor
+
+console.log(array[posicion]); //acceso a valor
+
+array[posicion] = "hola"; // reasigno el valor de la posicion
+
+console.log(array[posicion]);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers[0] = 10;
+numbers[2] = 30;
+console.log(numbers);
